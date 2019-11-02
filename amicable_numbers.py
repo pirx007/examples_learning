88bde621ff4ad3abb712e447c46c55857934bca0
pzoladz <pzoladz@gmail.com>
2019-11-02 21:51:52 +0100

    Prining proper divisors

--- amicable_numbers.py
+++ amicable_numbers.py
@@ -2,4 +2,7 @@ print("test")
 MAX_RANGE = 300
 
 for x in range(1, MAX_RANGE):
-    print(x)
+    print("For number" + str(x))
+    for y in range(1, x):
+        if x % y == 0:
+            print(y)
