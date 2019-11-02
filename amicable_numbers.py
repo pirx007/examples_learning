9bc1ae6b8574f132102e207cdefcf9ae01c0643d
pzoladz <pzoladz@gmail.com>
2019-11-02 23:04:04 +0100

    Fix summing dividers

--- amicable_numbers.py
+++ amicable_numbers.py
@@ -5,14 +5,12 @@ sum_of_y = 0
 for x in range(1, MAX_RANGE):
     print("===================")
     print("For number " + str(x))
+    sum_of_y = 0
     for y in range(1, x):
         if x % y == 0:
             print("x=" + str(x))
             print("y=" + str(y))
             right_divisor = y
-    sum_of_y =+ right_divisor
-            #ok_y = ok_y + y
-            #print("ok_y=" + str(ok_y))
-print("sum_of_y=" + str(sum_of_y))
- #print("div sum=" + str(ok_y))
+            sum_of_y = sum_of_y + y
+    print(sum_of_y)
             
