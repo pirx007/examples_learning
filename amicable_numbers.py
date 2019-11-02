22b44125002be826be604958a2d936739c975f06
pzoladz <pzoladz@gmail.com>
2019-11-02 22:37:44 +0100

    aproach to sum divisors

--- amicable_numbers.py
+++ amicable_numbers.py
@@ -1,8 +1,18 @@
-print("test")
-MAX_RANGE = 300
+MAX_RANGE = 10
+right_divisor = 0
+sum_of_y = 0
 
 for x in range(1, MAX_RANGE):
-    print("For number" + str(x))
+    print("===================")
+    print("For number " + str(x))
     for y in range(1, x):
         if x % y == 0:
-            print(y)
+            print("x=" + str(x))
+            print("y=" + str(y))
+            right_divisor = y
+    sum_of_y =+ right_divisor
+            #ok_y = ok_y + y
+            #print("ok_y=" + str(ok_y))
+print("sum_of_y=" + str(sum_of_y))
+ #print("div sum=" + str(ok_y))
+            
